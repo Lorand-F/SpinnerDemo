14980f328b0e43bda9dcb6bc3437211b450ffc5a
Lorand-F <formagiu.lorand@gmail.com>
2022-10-17 21:23:41 +0300

    Phase 2. Spinner.setAdapter(adapter).

--- app/src/main/java/com/example/spinneractivitydemo/MainActivity.java
+++ app/src/main/java/com/example/spinneractivitydemo/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.spinneractivitydemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    Spinner spinner;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner=findViewById(R.id.spinner);
+        imageView=findViewById(R.id.ivOutput);
+
+        String[] country=new String[]{"None", "EU", "Romania"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,country);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
